333ecf2e360f3d73b8eb48b4d2f3ffccb038c92e
Kat M <kmunson13@gmail.com>
2022-10-10 12:37:41 -0500

    Add book constructor

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const openModal = document.querySelectorAll('[data-modal-target]');
 const closeModal = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
+const checkBox = document.get
 
 openModal.forEach(button => 
     {button.addEventListener('click', () => {
@@ -28,5 +29,26 @@ function closeModalFunction(modal) {
 
 overlay.addEventListener('click', () => {
     const modals = document.querySelectorAll('.modal.active')
-    modals.forEach(modal => closeModal(modal))
+    modals.forEach(modal => closeModalFunction(modal))
 })
+
+function toggle() {
+    const readButton = document.querySelector('#if-read');
+    readButton.forEach(button => {button.addEventListener('click', () => {
+        readButton.classList.toggle('active')
+    })})}
+
+let array = []
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+    }
+    }
+
+  const book1 = new Book("The Hobbit", "JRR Tolkien", "256 pages")
+  const book2 = new Book("Song of Ice and Fire", "George RR Martin", "3,000 pages")
+  const book3 = new Book("Begin to Code with JAvascript", "Rob Miles", "500 pages")
